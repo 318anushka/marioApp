6da4c5fb7a4c3fd3841896e643197387425fd6c0
Anushka Singh <anushka318singh@gmail.com>
2020-08-06 04:01:16 +0530

    display details of a project

--- src/components/projects/Projectlist.js
+++ src/components/projects/Projectlist.js
@@ -1,16 +1,13 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
     return(
         <div className="project-list section">
-
-        <div className="card z-depth-0 project-summary">
-            <div className="card-content grey-text text-darken-3">
-            <span className="card-title ">Project title</span>
-            <p>Posted by Anushka</p>
-            <p className="grey-text">6th August</p>
-            </div>
-        </div>
+            <ProjectSummary></ProjectSummary>
+            <ProjectSummary></ProjectSummary>
+            <ProjectSummary></ProjectSummary>
+            <ProjectSummary></ProjectSummary>
       </div>
     )
 }
