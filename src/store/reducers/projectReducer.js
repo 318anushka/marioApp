1cc5ff3b23254ac78fc2f64f304a5f356b034d78
Anushka Singh <anushka318singh@gmail.com>
2020-08-17 12:47:58 +0530

    added project actions and modified dashboard

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,18 +1,16 @@
-const initState = {
-    projects: [
-        {id: '1', title: 'a', content: 'blah blah blah'},
-        {id: '2', title: 'b', content: 'blah blah blah'},
-        {id: '3', title: 'c', content: 'blah blah blah'}
-      ]
-    
-}
+const initState = {}
 
 const projectReducer = (state = initState, action) => {
   switch (action.type) {
-    case 'CREATE_PROJECT':
-      console.log('create project', action.project);
+    case 'CREATE_PROJECT_SUCCESS':
+      console.log('create project success');
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error');
+      return state;
+    default:
+      return state;
   }
-  return state;
 };
 
 export default projectReducer;
